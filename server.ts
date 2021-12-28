0b9a9da47a410b1e8385d496c72b8563b217c62e
mike connor <mike@rocketnumber9.org>
2021-12-28 16:09:11 -0600

    cors header and tsconfig

--- server.ts
+++ server.ts
@@ -3,10 +3,10 @@ const app = express()
 const port =3000
 
 app.get('/data', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.json({ foo: 'bar'})
 })
 
 app.listen(port, ()=>  {
-    console.log('listening on port ' + port);
-    
+    console.log('listening on port ' + port)
 })

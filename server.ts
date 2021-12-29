2933cbb906e64c79aa4a91bcfc6b77e67a6e46b2
mike connor <mike@rocketnumber9.org>
2021-12-29 07:58:34 -0600

    server type

--- server.ts
+++ server.ts
@@ -2,9 +2,16 @@ import express from 'express'
 const app = express()
 const port =3001
 
+export interface QueryPayload {
+    foo:string
+}
+
 app.get('/data', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.json({ foo: 'bar'})
+
+    const data: QueryPayload = { foo: 'bar' }
+    
+    res.json(data)
 })
 
 app.listen(port, ()=>  {
